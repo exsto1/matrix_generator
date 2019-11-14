f38feffebd05e9381a7c741a7a25314a40cb6b4a
exsto <exsto1@gmail.com>
2019-11-14 23:56:18 +0100

    Initial commit

---
+++ feature_matrix_gen.py
@@ -0,0 +1,46 @@
+# from matplotlib import pyplot as plt
+min_gap_size = 2
+sekwencje = open('representative_alignment.fasta').readlines()
+sekwencje = [i for i in sekwencje if '>' not in i]
+
+# Wersja precyzyjna z uwzględnianiem pojedynczych gapów.
+sectors_positions = []
+is_Gap = True
+gap_counter = 1
+for i in range(len(sekwencje)):
+    for i1 in range(len(sekwencje[i])):
+        if is_Gap:
+            if sekwencje[i][i1] != "-":
+                if gap_counter > 1:
+                    sectors_positions.append(i1)
+                    is_Gap = False
+                    gap_counter = 1
+                else:
+                    is_Gap = False
+                    gap_counter = 1
+            else:
+                gap_counter += 1
+        else:
+            if sekwencje[i][i1] == "-":
+                sectors_positions.append(i1)
+                is_Gap = True
+
+sectors_positions = list(dict.fromkeys(sectors_positions))
+sectors_positions.sort()
+
+matrix = []
+
+temp_list = []
+for i in range(len(sekwencje)):
+    for i1 in sectors_positions:
+        if sekwencje[i][i1] == "-":
+            temp_list.append(0)
+        else:
+            temp_list.append(1)
+    matrix.append(temp_list)
+    temp_list = []
+
+
+# a = plt.imshow(matrix)
+# plt.savefig("new1")
+# plt.show()
